d77973a1178135279b265cd353cdd29e6cfdf3aa
Gilang <gilang.adrian@outlook.com>
2021-11-02 00:15:40 +0700

    Add Pausemenu

---
+++ Assets/Scripts/PauseMenu/PauseGame.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }*/
+        
+    }
+
+  /*  void Resume();
+    void Pause();*/
+}
