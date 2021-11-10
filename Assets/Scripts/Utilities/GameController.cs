7a5f4401891138fc203286eb773c5187ee11ebf9
Adhella <adhellasbl@gmail.com>
2021-11-11 03:06:26 +0700

    Added coin mechanism with animation!
    - You can access any methods for coin manipulation in the GameController
    - For now, whenever a user finished a session, the coin gets added the amount of the session score
    - The coin prefab is ayas approved

--- Assets/Scripts/Utilities/GameController.cs
+++ Assets/Scripts/Utilities/GameController.cs
@@ -1,7 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
+// enum to define keys for using playerprefs
+enum PlayerDataKey
+{
+    CoinAmount,
+    Character,
+};
 
 // GameController is our Singleton Class that serves as the game's controller (duh)
 // It stores the game's global variable such as game currency and player's current skin (future development)
@@ -9,7 +16,10 @@ public class GameController : MonoBehaviour
 {
     public static GameController instance;
     private int totalCoin;
-    private int currentSkin; // currentSkin is a variable that holds skinId
+    private int currentCharacter; // currentSkin is a variable that holds current characterId
+    public GameObject[] coinAmount;
+    public GameObject[] coinChangeIndicator;
+
 
     private void Awake()
     {
@@ -39,35 +49,74 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        ShowCoinAmount();
     }
 
     // Initialize values from PlayerPrefs
     private void InitializeVariable()
     {
         // Both currentSkin and totalCoin default value is 0
-        this.currentSkin = PlayerPrefs.GetInt("CurrentSkin", 0);
-        this.totalCoin = PlayerPrefs.GetInt("TotalCoin", 0);
+        this.currentCharacter = PlayerPrefs.GetInt(PlayerDataKey.Character.ToString(), 0);
+        this.totalCoin = PlayerPrefs.GetInt(PlayerDataKey.CoinAmount.ToString(), 0);
+
+        ShowCoinAmount();
     }
 
-    void setTotalCoin(int newTotalCoinValue)
+    void ShowCoinAmount()
     {
-        this.totalCoin = newTotalCoinValue;
-        PlayerPrefs.SetInt("TotalCoin", newTotalCoinValue); // Automatically saves the new value to PlayerPrefs
+        // setting the value into the game object 
+        coinAmount = GameObject.FindGameObjectsWithTag("CoinAmount");
+
+        foreach (GameObject c in coinAmount)
+        {
+            c.GetComponent<TMP_Text>().text = totalCoin.ToString();
+        }
+
     }
 
-    void setCurrentSkin(int newCurrentSkin)
+    // animate the decrease or increase in coinAmount
+    // parameter:
+    // - string sign => a "+" or "-"
+    // - int amount => an integer that represents the amount increased or decreased
+    private void AnimateCoinChange(string sign, int amount)
     {
-        this.currentSkin = newCurrentSkin;
-        PlayerPrefs.SetInt("CurrentSkin", newCurrentSkin); // Automatically saves the new value to PlayerPrefs
+        coinChangeIndicator = GameObject.FindGameObjectsWithTag("CoinChangeIndicator");
+        foreach (GameObject c in coinChangeIndicator)
+        {
+            c.GetComponent<TMP_Text>().text = sign +""+amount.ToString();
+            AnimationUtilities.AnimateAddMoney(c); // calls on AnimationUtilities class
+            break;
+        }
+
     }
 
-    int getCurrentSkin()
+    // public function where you can add any amount of coin
+    public void CoinAdd(int coinAmount)
     {
-        return this.currentSkin;
+        AnimateCoinChange("+", coinAmount);
+        this.totalCoin += coinAmount;
+        PlayerPrefs.SetInt(PlayerDataKey.CoinAmount.ToString(), totalCoin); // Automatically saves the new value to PlayerPrefs
+    }
+    // public function where you can substract any amount of coin
+    public void CoinSubstract(int coinAmount)
+    {
+        AnimateCoinChange("-", coinAmount);
+        this.totalCoin -= coinAmount;
+        PlayerPrefs.SetInt(PlayerDataKey.CoinAmount.ToString(), totalCoin); // Automatically saves the new value to PlayerPrefs
     }
 
-    int getTotalCoin()
+    public void setCurrentSkin(int skinId)
+    {
+        this.currentCharacter = skinId;
+        PlayerPrefs.SetInt(PlayerDataKey.Character.ToString(), skinId); // Automatically saves the new value to PlayerPrefs
+    }
+
+    public int getCurrentSkin()
+    {
+        return this.currentCharacter;
+    }
+
+    public int getTotalCoin()
     {
         return this.totalCoin;
     }
