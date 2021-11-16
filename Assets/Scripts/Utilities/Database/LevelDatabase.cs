035bb7760ac86c48b1f9ae8824176b9f1c461739
adhxll <59019912+adhxll@users.noreply.github.com>
2021-11-16 09:40:05 +0700

    Revert "Merge pull request #7 from adhxll/game-scene/practice-mode/ayasrefactor"
    This reverts commit 7875e9bb8a07893535b590999c39786ee0d40424.

--- Assets/Scripts/Utilities/Database/LevelDatabase.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Level Database", menuName = "Assets/Database/Level")]
-public class LevelDatabase : ScriptableObject
-{
-    public List<Level> allLevels;
-}
