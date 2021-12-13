19d28c74d3f3d36a36cba2744e2260f82b82f67b
Samuel Maynard <samuelmaynard13@gmail.com>
2021-12-13 20:00:26 +0700

    Dona 2 Refactor UI Manager

--- Assets/Scripts/Settings/SUIManager.cs
+++ Assets/Scripts/Settings/SUIManager.cs
@@ -7,15 +7,20 @@ public class SUIManager : MonoBehaviour
     [SerializeField]
     private GameObject closeButton;
     [SerializeField]
-    private Gameobject saveButton;
+    private GameObject saveButton;
+    [SerializeField]
+    private SettingsController settingsController;
 
     public void CloseButton(){
         AudioController.Instance.PlaySound(SoundNames.click);
         AnimationUtilities.AnimateButtonPush(closeButton);
+        settingsController.CloseSettings();
     }
 
     public void SaveButton(){
         AudioController.Instance.PlaySound(SoundNames.click);
         AnimationUtilities.AnimateButtonPush(saveButton);
+        SettingsController.Instance.SaveAllSettings();
+        settingsController.CloseSettings();
     }
 }
