bde72f2c65a392b3aaa1afb7533b1e38c596431c
Samuel Maynard <samuelmaynard13@gmail.com>
2021-12-14 22:13:39 +0700

    Done waterfall sound adjustment

--- Assets/Scripts/LevelSelect/LevelAudioController.cs
+++ Assets/Scripts/LevelSelect/LevelAudioController.cs
@@ -4,47 +4,6 @@ using UnityEngine;
 
 public class LevelAudioController : MonoBehaviour
 {
-    [SerializeField] private AudioClip[] audioClip = null;
-    [SerializeField] private AudioSource audioSource = null;
-    [SerializeField] private float transitionDuration = 0;
-    private int currentIndex = 0;
-
-    private float maxVolume = 1f;
-    private float minVolume = 0f;
-
-    private void Awake()
-    {
-        currentIndex = GameController.Instance.currentStage;
-    }
-
-    public void ChangeAudioBackgroundForward(){
-        StartCoroutine(ChangeClip(audioClip[currentIndex+1]));
-        currentIndex += 1;
-    }
-
-    public void ChangeAudioBackgroundBackward(){
-        StartCoroutine(ChangeClip(audioClip[currentIndex-1]));
-        currentIndex -= 1;
-    }
-
-    private IEnumerator ChangeClip(AudioClip _audioClip){
-        for (float t=0f; t<transitionDuration; t+=Time.deltaTime){
-            audioSource.volume = Mathf.Lerp(audioSource.volume, minVolume, t / transitionDuration);
-            yield return null;
-        }
-        audioSource.Stop();
-        audioSource.clip = _audioClip;
-        audioSource.Play();
-
-        for (float t=0f; t<transitionDuration; t+=Time.deltaTime){
-            audioSource.volume = Mathf.Lerp(audioSource.volume, maxVolume, t / transitionDuration);
-            yield return null;
-        }
-    }
-
-    void Start(){
-        Destroy(GameObject.FindGameObjectWithTag("MainThemeSong"));
-    }
     
 }
 
