6d5868e98b79f6a61875d185afb2188fbd547ab2
Adhella <adhellasbl@gmail.com>
2022-02-04 17:55:15 +0700

    MCD-178: BE DevMode for skipping levels with desired results
    For now the script is in the level selection UI script :") But it's quite modular by itself so let's just put it anywhere else when the right time has come.

--- Assets/Scripts/LevelSelect/LSModalUIManager.cs
+++ Assets/Scripts/LevelSelect/LSModalUIManager.cs
@@ -52,4 +52,38 @@ public class LSModalUIManager : MonoBehaviour
     {
         GameController.Instance.sceneState = scene;
     }
+
+    // use this function whenever a play button is called to skip it duting developer mode
+    // It takes one parameter, star. You can skip levels with desired outcome based on star obtained using this function.
+    // if you put random number, it will just give you the 0 star results
+    // implement this to play, restart and next button.
+    private void SkipLevel(int star)
+    {
+        int totalScore = 0;
+        int totalNotes = 100;
+        int correctNotes = 0;
+        int accuracy = 0;
+        if (star == 3){
+            accuracy = 100;
+            correctNotes = 100;
+            totalScore = 10000;
+        }
+        else if (star == 2){
+            accuracy = 60;
+            correctNotes = 60;
+            totalScore = 1000;
+        }
+        else if (star == 1){
+            accuracy = 30;
+            correctNotes = 30;
+            totalScore = 100;
+        }
+
+        PlayerPrefs.SetInt("SessionScore", totalScore);
+        PlayerPrefs.SetInt("SessionTotalNotes", (int)totalNotes);
+        PlayerPrefs.SetInt("SessionCorrectNotes", (int)correctNotes); 
+        PlayerPrefs.SetInt("SessionAccuracy", accuracy);
+
+        SceneManagerScript.Instance.SceneInvoke(SceneManagerScript.SceneName.ResultPage);
+    }
 }
